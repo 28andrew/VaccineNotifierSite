ee3c47d917ca5b17c362e444787206e2e1af3435
28andrew <andrewtran312@gmail.com>
2021-04-04 21:42:31 -0500

    Add Config.php which was incorrectly gitignored

---
+++ src/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace VaccineNotifier;
+
+
+use Exception;
+
+class Config {
+    public static function get($path) {
+        if(!@include($_SERVER["DOCUMENT_ROOT"] . "/config.php")) throw new Exception("Missing config.php. Copy config.example.php to config.php.");
+        if (!@$config) throw new Exception("Missing $config object in config.php");
+
+        $arr = $config;
+        $keys = explode(".", $path);
+        foreach ($keys as $key) {
+            $arr = &$arr[$key];
+        }
+        return $arr;
+    }
+}
