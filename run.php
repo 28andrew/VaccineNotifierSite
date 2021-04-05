c777cd91d93eda679ffc97ed9bc8d928782225fa
28andrew <andrewtran312@gmail.com>
2021-04-04 21:56:27 -0500

    Avoid caching in run.php

--- run.php
+++ run.php
@@ -21,6 +21,11 @@ if ($ip !== Config::get('self_ip')) {
     return;
 }
 
+// Avoid caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 // Find states of users
 Database::connect();
 $states = DB::queryFirstColumn("SELECT DISTINCT `state` FROM `users`");
